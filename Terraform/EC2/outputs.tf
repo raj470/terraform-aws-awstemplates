53018f8fcba8ecbb2b823f717387a862798ae316
raj470 <srajeshwarreddy001@gmail.com>
2024-08-03 23:22:48 -0400

    Updated outputs files

--- Terraform/EC2/outputs.tf
+++ Terraform/EC2/outputs.tf
@@ -1,12 +1,12 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app_server.id
+  value       = aws_instance.terraform.id
 }
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  value       = aws_instance.terraform.public_ip
 }
 output "vpc_security_group_ids" {
   description = "Security group ID"
-  value = aws_instance.app_server.vpc_security_group_ids
+  value = aws_instance.terraform.vpc_security_group_ids
 }
