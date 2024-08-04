d3e8df64d6e6fd741e68cfb85a2e5d4787ae14a6
raj470 <srajeshwarreddy001@gmail.com>
2024-08-04 15:42:01 -0400

    testing the terraform private registry

---
+++ Terraform/main.tf
@@ -0,0 +1,21 @@
+# Terraform Configuration
+terraform {
+   required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = ">=5.56.1"
+      }
+    }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "temp" {
+  source  = "app.terraform.io/raj_aws/temp/aws"
+  version = "0.0.1"
+#   credentials "app.terraform.io" {
+#   # valid user API token
+#   token = "xxxxxx.atlasv1.zzzzzzzzzzzzz"
+# }
+}
