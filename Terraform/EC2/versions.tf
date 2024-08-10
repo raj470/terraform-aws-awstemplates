e9a67b053b1668e42453ddcb8ab26fdc7c6e5a8e
raj470 <srajeshwarreddy001@gmail.com>
2024-08-10 19:21:32 -0400

    modified files

--- Terraform/EC2/versions.tf
+++ Terraform/EC2/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-    cloud{
-      organization = "raj_aws"
-        workspaces {
-          name = "awstemplates"
-        }
-    }    
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = ">=5.56.1"
-      }
+  cloud {
+    organization = "raj_aws"
+    workspaces {
+      name = "awstemplates"
     }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.56.1"
+    }
+  }
 }
