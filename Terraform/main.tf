bcf92bc5dd014eea471681988562361ba5900dfd
raj470 <srajeshwarreddy001@gmail.com>
2024-08-14 07:57:59 -0400

    formatted the syntax

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,11 +8,25 @@ terraform {
   }
 }
 provider "aws" {
-  region = "us-east-1"
+  region                   = "us-east-1"
   shared_credentials_files = ["/Users/rajeshwarreddysirigada/.aws/credentials"]
 }
 
 module "temp" {
   source  = "app.terraform.io/raj_aws/temp/aws"
   version = "0.0.3"
+}
+
+locals {
+  instance_type = "t2.micro"
+  ami = "ami-0a1179631ec8933d7"
+  tag = "locals_provision"
+}
+
+resource "aws_instance" "ec2_locals" {
+  instance_type = "${local.instance_type}"
+  ami = "${local.ami}"
+  tags = {
+    Name = "${local.tag}-tag"
+  }
 }
