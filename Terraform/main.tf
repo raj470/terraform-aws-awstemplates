3e3eed96f6a9611c20b7f3d799d164aa25c5bf04
raj470 <srajeshwarreddy001@gmail.com>
2024-08-15 10:17:22 -0400

    modified file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -16,17 +16,3 @@ module "temp" {
   source  = "app.terraform.io/raj_aws/temp/aws"
   version = "0.0.3"
 }
-
-locals {
-  instance_type = "t2.micro"
-  ami = "ami-0a1179631ec8933d7"
-  tag = "locals_provision"
-}
-
-resource "aws_instance" "ec2_locals" {
-  instance_type = "${local.instance_type}"
-  ami = "${local.ami}"
-  tags = {
-    Name = "${local.tag}-tag"
-  }
-}
