f21db92be4f9111226c444a8a64dea592a9c2485
raj470 <srajeshwarreddy001@gmail.com>
2024-11-10 11:47:15 -0500

    S3 code for folder name

--- Terraform/Lambda/python_code/s3_code.py
+++ Terraform/Lambda/python_code/s3_code.py
@@ -1,20 +1,18 @@
 import json
 import boto3
 from datetime import datetime, timedelta, timezone
-def get_last_modified(object):
-    return int(object['LastModified'].strftime('%s'))
 def lambda_handler(event, context):
     S3 = boto3.client('s3')
     list_bucket = S3.list_buckets()
-    for bucket in list_bucket['Buckets']:
-        print(bucket['Name'])
-    list_objects = S3.list_objects_v2(Bucket='bucket123cross')
+    bucket_name = "bucket123cross"
     utc_minus_4 = timezone(timedelta(hours=-4))
-    # for object in list_objects['Contents']:
-    #     print(object['Key'])
-    last_modified = S3.list_objects_v2(Bucket='bucket123cross')
-    # for object in list_objects['Contents']:
-    #     print(object['LastModified'])
     if 'Contents' in last_modified:
         for object in last_modified['Contents']:
-            print(bucket['Name']+" "+object['Key'] + " " + object['LastModified'].astimezone(utc_minus_4).strftime('%Y-%m-%d %H:%M:%S %Z'))
+            last_modified = object['LastModified'].astimezone(utc_minus_4)
+            folder_name = last_modified.strftime("%Y-%m-%d %H:%M:%S %Z")
+            new_key = f"{folder_name}/{object['Key'].split('/')[-1]}"  
+            S3.copy_object(
+                Bucket=bucket_name,
+                CopySource={'Bucket': bucket_name, 'Key': object['Key']},
+                Key=new_key
+            )
