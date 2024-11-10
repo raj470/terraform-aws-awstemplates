7cab800b0f67cd59353ddbaf929b26cbc265a78b
raj470 <srajeshwarreddy001@gmail.com>
2024-11-09 22:35:51 -0500

    terraform for lambda s3 trigger

---
+++ Terraform/Lambda/python_code/s3_code.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+import datetime
+def get_last_modified(object):
+    return int(object['LastModified'].strftime('%s'))
+def lambda_handler(event, context):
+    S3 = boto3.client('s3')
+    list_bucket = S3.list_buckets()
+    for bucket in list_bucket['Buckets']:
+        print(bucket['Name'])
+    list_objects = S3.list_objects_v2(Bucket='bucket123cross')
+    # for object in list_objects['Contents']:
+    #     print(object['Key'])
+    last_modified = S3.list_objects_v2(Bucket='bucket123cross')
+    # for object in list_objects['Contents']:
+    #     print(object['LastModified'])
+    if 'Contents' in last_modified:
+        for object in last_modified['Contents']:
+            print(bucket['Name']+" "+object['Key'] + " " + str(object['LastModified']))
---
+++ Terraform/Lambda/s3trigger.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["/Users/rajeshwarreddysirigada/.aws/credentials"]
+}
+
+resource "aws_lambda_function" "s3_lambda_trigger" {
+  function_name = "s3_time_catz"
+  role          = "arn:aws:iam::058264186519:role/lambdaexecutionrole"
+  architectures = ["x86_64"]
+  runtime       = "python3.12"
+  handler       = "s3_code.lambda_handler"
+  filename      = "${path.module}/python_code/s3_code.zip"
+}
+
+data "archive_file" "zip_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/python_code/"
+  output_path = "${path.module}/python_code/s3_code.zip"
+}
